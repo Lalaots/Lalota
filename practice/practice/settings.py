7de42ee00916d8415ec28db969e4b90699030628
Lalaots <lalotsvvirays@gmail.com>
2025-12-09 21:22:37 +0800

    Update settings.py

--- practice/practice/settings.py
+++ practice/practice/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['yourapp.onrender.com']
 
 
 # Application definition
@@ -127,5 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
