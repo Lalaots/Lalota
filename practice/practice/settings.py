86810a8d121f7ca304a2c1dbf83eeef9679ae036
Lalaots <lalotsvvirays@gmail.com>
2025-12-09 21:16:58 +0800

    Update settings.py

--- practice/practice/settings.py
+++ practice/practice/settings.py
@@ -30,7 +30,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
@@ -127,4 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
